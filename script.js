52fc1c22b4d0f6e26de57938d7a391e596c275e6
sasbsr <152930818+sasbsr@users.noreply.github.com>
2023-12-05 22:29:37 +0100

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function openTab(tabName) {
-    // Hide all tabs
-    document.getElementById('about').style.display = 'none';
-    document.getElementById('collections').style.display = 'none';
-    document.getElementById('experience').style.display = 'none';
-
-    // Show the selected tab
-    document.getElementById(tabName).style.display = 'block';
-}
